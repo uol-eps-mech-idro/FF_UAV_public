b474d1f365f5e6e5690a96285d083e15652f60e4
Lenka Mudrich <lenka.mudrich@gmail.com>
2023-07-26 20:12:16 +0100

    Separate msgs to its own package

--- battery_monitor/src/battery_monitor.cpp
+++ battery_monitor/src/battery_monitor.cpp
@@ -7,8 +7,8 @@
 #include "std_msgs/Bool.h"
 #include "std_msgs/String.h"
 
-#include "battery_monitor/BatteryStatus.h"
-#include "battery_monitor/InputAccepted.h"
+#include "uav_msgs/BatteryStatus.h"
+#include "uav_msgs/InputAccepted.h"
 
 ros::Publisher battery_status_pub;
 ros::Publisher input_status_pub;
@@ -23,27 +23,27 @@ constexpr int8_t kMinBatteryLevel {0};
 
 void BatteryCallback(const sensor_msgs::BatteryState::ConstPtr& msg)
 {
-  battery_monitor::InputAccepted msg2;
+  uav_msgs::InputAccepted msg2;
   msg2.data = true;
   msg2.input_msg_id = msg->header.seq;
   input_status_pub.publish(msg2);
 
-  battery_monitor::BatteryStatus output_msg;
+  uav_msgs::BatteryStatus output_msg;
   output_msg.input_msg_id = msg->header.seq;
-  output_msg.status = battery_monitor::BatteryStatus::UNSET;
+  output_msg.status = uav_msgs::BatteryStatus::UNSET;
 
   if ((safety_threshold != -1) && (mission_threshold != -1)) { //the thresholds must be set for the status to be published
     int8_t current_battery {(int8_t)round(msg->percentage*100)};
 
     if ((current_battery > mission_threshold) && (current_battery <= kMaxBatteryLevel)){
-      output_msg.status = battery_monitor::BatteryStatus::OK;
+      output_msg.status = uav_msgs::BatteryStatus::OK;
     } else if ((current_battery <= mission_threshold) && (current_battery > safety_threshold )) {
-      output_msg.status = battery_monitor::BatteryStatus::MISSION_CRITICAL;
+      output_msg.status = uav_msgs::BatteryStatus::MISSION_CRITICAL;
     } else if ((current_battery <= safety_threshold) && (current_battery >= kMinBatteryLevel)) {
-      output_msg.status = battery_monitor::BatteryStatus::SAFETY_CRITICAL;
+      output_msg.status = uav_msgs::BatteryStatus::SAFETY_CRITICAL;
     } else {
       ROS_ERROR("Invalid battery reading.");
-      output_msg.status = battery_monitor::BatteryStatus::BATTERY_READING_OUT_OF_SCOPE;
+      output_msg.status = uav_msgs::BatteryStatus::BATTERY_READING_OUT_OF_SCOPE;
     }
   }
   battery_status_pub.publish(output_msg);
@@ -78,8 +78,8 @@ int main(int argc, char **argv)
     ros::shutdown();
   }
 
-  input_status_pub = nh.advertise<battery_monitor::InputAccepted>("input_accepted",1000);
-  battery_status_pub = nh.advertise<battery_monitor::BatteryStatus>("battery_status", 1000);
+  input_status_pub = nh.advertise<uav_msgs::InputAccepted>("input_accepted",1000);
+  battery_status_pub = nh.advertise<uav_msgs::BatteryStatus>("battery_status", 1000);
 
   ros::Subscriber battery_sub = nh.subscribe("/fcs/battery_state", 10, BatteryCallback);
 
